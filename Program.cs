0ab7886de1548587ec96b3c2a25105109db0fef1
Z <161516388+Ziphour@users.noreply.github.com>
2024-04-26 22:15:52 +0100

    Commited first C# repository on if statements and testing

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
+
+Random dice = new();
+int rollOne = dice.Next(1,3);
+int rollTwo = dice.Next(1,3);
+int rollThree = dice.Next(1,3);
+int totalRoll = rollOne + rollTwo + rollThree;
+
+Console.WriteLine(@$"Your total roll was {totalRoll}");
+
+if (totalRoll <= 4){
+
+Console.WriteLine($"Love ya!\n");
+
+
+} if ( totalRoll >= 5){
+
+    Console.WriteLine($"You win !\n");
+
+} if ((rollOne == rollTwo) && (rollThree == rollOne) ){
+
+    Console.WriteLine("Wow congrats! Secret line unlocked!!!\nTriples!!!");
+    totalRoll +=3;
+
+} else if ((rollOne == rollTwo) || (rollThree == rollOne) || (rollTwo == rollThree)){
+
+    Console.WriteLine("Wow congrats! Secret line unlocked!!!\nDoubles!!");
+    totalRoll += 2;
+
+}
+
+
+
+// Console.WriteLine(dice.Next((int)n1,(int)n2));
+// Console.WriteLine(dice.Next(9));
+
+// int n3 = dice.Next(1, 0);    
