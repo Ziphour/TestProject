bf0fd5796eae2b367e0912f7f1e48269ecc81b02
Z <161516388+Ziphour@users.noreply.github.com>
2024-04-27 18:55:38 +0100

    C sharp test

--- Program.cs
+++ Program.cs
@@ -1,39 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.ComponentModel.Design;
-using System.Text.RegularExpressions;
+﻿int[] fraudlentIDs = [1, 2, 3];
 
-Random dice = new();
-int rollOne = dice.Next(1,3);
-int rollTwo = dice.Next(1,3);
-int rollThree = dice.Next(1,3);
-int totalRoll = rollOne + rollTwo + rollThree;
+// fraudlentIDs[0] = "A123";
+// fraudlentIDs[1] = "b123";
 
-Console.WriteLine(@$"Your total roll was {totalRoll}");
+// fraudlentIDs = ["1","2","3"];
+Console.WriteLine(fraudlentIDs[1]);
+// fraudlentIDs[2] = 4;
+Console.WriteLine($"Reassign Third: {fraudlentIDs[2]} {fraudlentIDs.Length} ");
 
-if (totalRoll <= 4){
-
-Console.WriteLine($"Love ya!\n");
-
-
-} if ( totalRoll >= 5){
-
-    Console.WriteLine($"You win !\n");
-
-} if ((rollOne == rollTwo) && (rollThree == rollOne) ){
-
-    Console.WriteLine("Wow congrats! Secret line unlocked!!!\nTriples!!!");
-    totalRoll +=3;
-
-} else if ((rollOne == rollTwo) || (rollThree == rollOne) || (rollTwo == rollThree)){
-
-    Console.WriteLine("Wow congrats! Secret line unlocked!!!\nDoubles!!");
-    totalRoll += 2;
+int totalIDs = 0;
+int bin = 0;
+foreach (int id in fraudlentIDs)
+{
+    bin++;
+    // Console.WriteLine($"Value of :{id}\nNumber:{bin}");
+    totalIDs += id;
 
 }
-
-
-
-// Console.WriteLine(dice.Next((int)n1,(int)n2));
-// Console.WriteLine(dice.Next(9));
-
-// int n3 = dice.Next(1, 0);    
+Console.WriteLine("ID " + totalIDs + " bin " + bin);
